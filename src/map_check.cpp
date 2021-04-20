461baf7ce6bea24e5945a49ba8a19bdebab5064f
Namihei Adachi <a1721601@sendai-nct.jp>
2021-04-20 23:52:56 +0900

    image&matrix

--- src/map_check.cpp
+++ src/map_check.cpp
@@ -82,19 +82,6 @@ double MapCheck::normalizeAngle(double angle)
 }
 
 nav_msgs::msg::OccupancyGrid MapCheck::getMap(cv::Mat cost_mat){
-  //std::cout<<"paiotu"<<std::endl;
-  // std::cout << "cost_mat=" << cost_mat << std::endl << "ch=" << cost_mat.channels() << std::endl << std::endl;
-  // cv::Mat cost_map = cost_mat.reshape(1,1);
-  // std::cout << "cost_map=" << cost_map << std::endl << "ch=" << cost_map.channels() << std::endl << std::endl;
-  // std::cout << "size[]:" << cost_map.size().width << "," << cost_map.size().height << std::endl;
-  // std::vector<int>vec(cost_map.begin<int>(), cost_map.end<int>());
-  // int *d_arr = &vec[0];
-  // std::cout << "vec=";
-  // for(int i=0;i<12;i++){
-  //   std::cout << "," << d_arr[i];
-  // }
-  // std::cout << std::endl;
-
   //map出力
   float res=0.1;
   geometry_msgs::msg::Pose origin;
@@ -114,18 +101,12 @@ nav_msgs::msg::OccupancyGrid MapCheck::getMap(cv::Mat cost_mat){
   map.info.origin = origin;//geometry_msgs/Pose
   rclcpp::Clock ros_clock(RCL_ROS_TIME);
   map.info.map_load_time = ros_clock.now();
-  //ここどうにかする
-  // for(int i=1;i<=cost_mat.size().width*cost_mat.size().height;i++){
-  //   map.data.push_back(d_arr[i]);//int8(0~100)
-  // }
-  // std::cout<<"0"<<std::endl;
   map.data.resize(cost_mat.size().width*cost_mat.size().height);
-  // std::cout<<"1"<<std::endl;
-  //std::cout << "size[]:" << cost_mat.size().width << "," << cost_mat.size().height << std::endl;
   int dc=0;
   for (int y = 0; y < cost_mat.size().height; y++){
     for (int x = 0; x < cost_mat.size().width; x++){
-      map.data[dc]=100*cost_mat.at<unsigned char>(y,x)/255;
+      map.data[dc]=100*cost_mat.at<unsigned char>(y,x)/255;//img
+      //map.data[dc]=cost_mat.at<int>(y,x);//mat
       dc++;
     }
   }
@@ -139,8 +120,6 @@ void MapCheck::TimerCallback()
     cv::Mat img = cv::imread("/home/adachi/Lena_gray.jpg");
     if (img.empty() == false) {
       cv::Mat cost_mat;
-      //std::cout << "size[]:" << img.size().width << "," << img.size().height << std::endl;
-      // グレースケールに変換する
       cv::cvtColor(img, cost_mat,cv::COLOR_BGR2GRAY);
       //cvShowImage("opencv_logo", cost_mat);
       this->pub_map->publish(MapCheck::getMap(cost_mat));
